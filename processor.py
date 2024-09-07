ba4854b0c082b2210e7e7cd95dd732cfda978c1a
1ikeadragon <40909973+1ikeadragon@users.noreply.github.com>
2024-09-07 08:06:18 +0530

    added denylist logic

--- processor.py
+++ processor.py
@@ -12,15 +12,27 @@ name = csv_file.rsplit('.', 1)[0]
 
 clean_domain = lambda domain: re.sub(r'^https?://', '', domain).split('/')[0]
 
+allowlist = []
+denylist = []
+
 with open(csv_file, mode='r') as file:
     csv_reader = csv.DictReader(file)
-    allowlist = [
-        f"*.{clean_domain(row['identifier'])}" 
-        for row in csv_reader 
-        if row['identifier'] and row['asset_type'] == "URL"
-    ]
+    
+    for row in csv_reader:
+        domain = clean_domain(row['identifier'])
+        if row['identifier'] and row['asset_type'] == "URL":
+            if row['eligible_for_submission'].lower() == 'true':
+                allowlist.append(f"*.{domain}")
+            elif row['eligible_for_submission'].lower() == 'false':
+                denylist.append(f"*.{domain}")
 
-scope_data = {"id": "1", "name": name, "allowlist": allowlist, "denylist": []}
+scope_data = {
+    "id": "1",
+    "name": name,
+    "allowlist": allowlist,
+    "denylist": denylist
+}
+
 json_filename = f"{name}.json"
 with open(json_filename, 'w') as jsonf:
     json.dump(scope_data, jsonf, indent=4)
